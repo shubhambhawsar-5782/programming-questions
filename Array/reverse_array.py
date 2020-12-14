f0065aa819127682cb1aaa0a53d6834e8ad2ee3f
shubhambhawsar-5782 <51023390+shubhambhawsar-5782@users.noreply.github.com>
2020-12-14 23:15:44 +0530

    add recursive methode

--- Array/reverse_array.py
+++ Array/reverse_array.py
@@ -7,3 +7,19 @@ def reverse(l,start,end):
 l=list(map(int,input().split()))
 reverse(l,0,len(l)-1)
 print(l)
+#Time Complexity : O(n)
+
+
+# Recursive python program to reverse an array
+"""def reverseList(A, start, end):
+    if start >= end:
+        return
+    A[start], A[end] = A[end], A[start]
+    reverseList(A, start+1, end-1)
+ 
+# Driver function to test above function
+A = [1, 2, 3, 4, 5, 6]
+print(A)
+reverseList(A, 0, 5)
+print("Reversed list is")
+print(A)"""
